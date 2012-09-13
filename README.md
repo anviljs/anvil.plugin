d31b0e4e2825dc2c883828ea4a3ea02078ab310e
Alex Robson <asrobson@gmail.com>
2012-09-13 00:46:23 -0500

    Initial check-in

---
+++ README.md
@@ -0,0 +1,8 @@
+## Anvil Plugin ... Plugin
+### yes, really
+
+This plugin is a core component of anvil and is required to function as expected.
+
+## Installation
+
+anvil will install this plugin during post-install.
---
+++ src/plugin.js
@@ -0,0 +1,34 @@
+var path = require( "path" );
+
+module.exports = function( _, anvil ) {
+	return anvil.plugin( {
+		name: "anvil.plugin",
+		commander: [
+			[ "disable [value]", "Disable plugin" ],
+			[ "enable [value]", "Enable plugin" ],
+			[ "install [value]", "Install a plugin from npm" ],
+			[ "list", "List available plugins" ],
+			[ "uninstall [value]", "Uninstall plugin" ]
+		],
+		commands: [ "disable", "enable", "install", "list", "uninstall" ],
+
+		configure: function( config, commander, done ) {
+			var self = this,
+				commands = _.chain( this.commands )
+							.filter( function( arg ) { return commander[ arg ]; } )
+							.map( function( arg ) {
+								var value = commander[ arg ];
+								return { command: arg, value: value };
+							} )
+							.value(),
+				command = commands.length === 0 ? undefined : commands[ 0 ];
+			if( command ) {
+				anvil.pluginManager[ command.command ]( command.value, function() {
+					anvil.events.raise( "all.stop", 0 );
+				} );
+			} else {
+				done();
+			}
+		}
+	} );
+};
